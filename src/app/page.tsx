d312815ef35d77d1b8c2c56be97bae34a5650f6e
isalim079 <salimreza1025@gmail.com>
2024-09-30 12:05:43 +0600

    Footer section created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from '@/components/shared/Navbar/Navbar'
 import HomePage from '@/components/Home/Home'
+import Footer from '@/components/shared/Footer/Footer';
 
 export default function Home() {
   return (
     <div>
      <Navbar />
      <HomePage />
+     <Footer />
     </div>
   );
 }
