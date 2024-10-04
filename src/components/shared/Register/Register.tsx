35504d1c02711f1926eab6d6434c6605c4d84916
isalim079 <salimreza1025@gmail.com>
2024-10-04 18:59:49 +0600

    Next auth setup done

--- src/components/shared/Register/Register.tsx
+++ src/components/shared/Register/Register.tsx
@@ -6,10 +6,16 @@ import Link from "next/link";
 
 import { FaFileUpload } from "react-icons/fa";
 
+export type UserData = {
+  name: string;
+  email: string;
+  password: string;
+  image: string;
+  role: "admin" | "user";
+  address: string;
+};
+
 const Register = () => {
-
-
-
   return (
     <div className="font-poppins overflow-hidden">
       <div className="relative -z-50">
@@ -34,7 +40,6 @@ const Register = () => {
 
               {/* Input container */}
               <div className="space-y-4 mt-10 ">
-
                 {/* prettier-ignore */}
                 <motion.input
                   type=""  placeholder="Name *" className="p-3 rounded-md w-full bg-primary-white/20 text-primary-white placeholder-primary-white" 
@@ -43,26 +48,44 @@ const Register = () => {
                   transition={{ duration: 1, delay: 0.3, type: "spring", }}
                 />
                 <motion.input
-                  type="email"  placeholder="Email *" className="p-3 rounded-md w-full bg-primary-white/20 text-primary-white placeholder-primary-white" 
-                  initial={{ x: -1750 }}
-                  animate={{ x: 0, }}
-                  transition={{ duration: 1, delay: 0.3, type: "spring", }}
-                  
+                  type="email"
+                  placeholder="Email *"
+                  className="p-3 rounded-md w-full bg-primary-white/20 text-primary-white placeholder-primary-white"
+                  initial={{ x: 1750 }}
+                  animate={{ x: 0 }}
+                  transition={{ duration: 1, delay: 0.3, type: "spring" }}
                 />
                 <motion.div
                   initial={{ x: -1750 }}
-                  animate={{ x: 0, }}
-                  transition={{ duration: 1, delay: 0.3, type: "spring", }}
+                  animate={{ x: 0 }}
+                  transition={{ duration: 1, delay: 0.3, type: "spring" }}
                   className=" relative rounded-md bg-primary-white/20"
                 >
-                <input
-                  type="file"  placeholder="Picture *" className="p-3 rounded-md w-full opacity-0 relative z-20 cursor-pointer"  
-                />
-                    <div className="text-primary-white flex items-center gap-5 absolute inset-0 left-3">
+                  <input
+                    type="file"
+                    placeholder="Picture *"
+                    className="p-3 rounded-md w-full opacity-0 relative z-20 cursor-pointer"
+                  />
+                  <div className="text-primary-white flex items-center gap-5 absolute inset-0 left-3">
                     <FaFileUpload className="text-3xl " />
                     <p>Upload profile picture</p>
-                    </div>
+                  </div>
                 </motion.div>
+
+                <motion.input
+                  initial={{ x: -1750 }}
+                  animate={{
+                    x: 0,
+                  }}
+                  transition={{
+                    duration: 1,
+                    delay: 0.6,
+                    type: "spring",
+                  }}
+                  type="text"
+                  placeholder="Address *"
+                  className="p-3 rounded-md w-full bg-primary-white/20 text-primary-white placeholder-primary-white"
+                />
 
                 <motion.input
                   initial={{ x: 1750 }}
